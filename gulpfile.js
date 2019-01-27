9a3798846c4ab92ead6468d063131138aa693614
FANTAZER <kuznecovs45@gmail.com>
2019-01-27 19:21:33 +0300

    add calc

--- gulpfile.js
+++ gulpfile.js
@@ -231,7 +231,8 @@ gulp.task('min:js',function(){
 // ########## make html ###############
 
 gulp.task('pug', function() {
-		gulp.src(['app/html/*.pug','app/module/**/*.pug',])
+		//gulp.src(['app/html/*.pug','app/module/**/*.pug',])
+		gulp.src(['app/html/calc.pug'])
 				//.pipe(changed('app/', {extension: '.html'}))
 				//.pipe(cache('pug'))
 				.pipe(pugInheritance({basedir: 'app/html/',skip:'node_modules/'}))
