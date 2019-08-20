19d1a26d5856f4a3b217dbfd578ae018b18c6b29
FANTAZER <kuznecovs45@gmail.com>
2019-08-20 16:47:02 +0300

    add page zone

--- gulpfile.js
+++ gulpfile.js
@@ -231,8 +231,8 @@ gulp.task('min:js',function(){
 // ########## make html ###############
 
 gulp.task('pug', function() {
-		//gulp.src(['app/html/*.pug','app/module/**/*.pug',])
-		gulp.src(['app/html/incomplect.pug'])
+		gulp.src(['app/html/*.pug','app/module/**/*.pug'])
+		//gulp.src(['app/html/sanitarzone.pug'])
 		//.pipe(changed('app/', {extension: '.html'}))
 				//.pipe(cache('pug'))
 				.pipe(pugInheritance({basedir: 'app/html/',skip:'node_modules/'}))
